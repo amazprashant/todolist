6f23e7f3c7bca5785c70df3f5eeac0ff344932c4
amazprashant <amazprashant@gmail.com>
2023-06-07 06:34:20 +0530

    migration update

--- database/migrations/2023_06_06_011737_todo.php
+++ database/migrations/2023_06_06_011737_todo.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todo', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('ctaegory_id')->nullable(false);
+            $table->unsignedInteger('category_id')->nullable(false);
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
         });
